a0abf0432f05f6a9bfa5a31886c1fbea8e0eb527
edvinahlgren <ahlgrenedvin@gmail.com>
2020-06-22 09:22:10 +0200

    Add basic header component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return (
+        <img src={logo} className="App-logo" alt="logo" />
+  );
+}
+
+export default Header;
